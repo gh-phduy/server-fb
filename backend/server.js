60e3d48a5737980441ae321d263ee62fed3e9887
Phuong Duy <evnnpd@gmail.com>
2024-07-01 05:49:15 +0700

    Facebook Back-end Version 1.2

--- backend/server.js
+++ backend/server.js
@@ -42,14 +42,6 @@ app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
-
 server.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 	connectMongoDB();
