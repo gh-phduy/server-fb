afe4bcb13b0d8ce07610b0dc5cac62c004a60158
Phuong Duy <evnnpd@gmail.com>
2024-07-09 17:11:36 +0700

    Test Server

--- backend/server.js
+++ backend/server.js
@@ -24,17 +24,24 @@ cloudinary.config({
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-	origin: [`${process.env.CLIENT_HOSTNAME}`, `${process.env.CLIENT_HOSTNAME_2}`],
-	credentials: true
-}))
+// app.use(cors({
+// 	origin: [`${process.env.CLIENT_HOSTNAME}`, `${process.env.CLIENT_HOSTNAME_2}`],
+// 	credentials: true
+// }))
+
+app.use(cors())
+
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
 
-
+app.use("/api/hello", (req, res) => {
+	res.json({
+		status: "Success"
+	})
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
