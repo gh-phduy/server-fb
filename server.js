c77fca8b5c67e6713eda700fdb7a0e8f24fc6a1e
Phuong Duy <evnnpd@gmail.com>
2024-07-10 12:14:32 +0700

    Fix CORS

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ app.use(cors({
 	credentials: true
 }))
 
-app.use(cors())
-
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
